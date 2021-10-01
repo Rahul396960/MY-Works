42a920eed97b12151428b6e9504af14d51ccd3dc
Rahul396960 <rahulsharma396960@gmail.com>
2021-10-01 18:12:11 +0530

    hey

---
+++ tracking.js
@@ -0,0 +1,62 @@
+
+function daily()
+{
+    document.getElementById('dailys').style.color = 'white';
+    document.getElementById('dailys').style.fontWeight='600';
+    document.getElementById('weeklys').style.color = 'hsl(236, 100%, 87%)';
+    document.getElementById('monthlys').style.color = 'hsl(236, 100%, 87%)';
+    document.getElementById('use1').innerHTML="5hrs ";
+    document.getElementById('used1').innerHTML="Previous - 7hrs ";
+    document.getElementById('use2').innerHTML="1hr";
+    document.getElementById('used2').innerHTML="Previous - 2hrs ";
+    document.getElementById('use3').innerHTML="0hrs";
+    document.getElementById('used3').innerHTML="Previous - 1hr";
+    document.getElementById('use4').innerHTML="1hr";
+    document.getElementById('used4').innerHTML="Previous - 1hr ";
+    document.getElementById('use5').innerHTML="1hr";
+    document.getElementById('used5').innerHTML="Previous - 3hrs ";
+    document.getElementById('use6').innerHTML="0hrs";
+    document.getElementById('used6').innerHTML="Previous - 1hr ";
+
+}
+
+function weekly()
+{
+    document.getElementById('dailys').style.color = 'hsl(236, 100%, 87%)';
+    document.getElementById('weeklys').style.fontWeight='600';
+    document.getElementById('weeklys').style.color = 'white';
+    document.getElementById('monthlys').style.color = 'hsl(236, 100%, 87%)';
+    document.getElementById('use1').innerHTML=" 32hrs ";
+    document.getElementById('used1').innerHTML="Previous - 36hrs ";
+    document.getElementById('use2').innerHTML="10hrs";
+    document.getElementById('used2').innerHTML=" Previous - 8hrs";
+    document.getElementById('use3').innerHTML="4hrs";
+    document.getElementById('used3').innerHTML="Previous - 7hrs";
+    document.getElementById('use4').innerHTML="4hrs";
+    document.getElementById('used4').innerHTML="Previous - 5hrs";
+    document.getElementById('use5').innerHTML="5hrs";
+    document.getElementById('used5').innerHTML="Previous - 10hrs";
+    document.getElementById('use6').innerHTML="2hrs ";
+    document.getElementById('used6').innerHTML="Previous - 2hrs";}
+
+function monthly()
+{
+    document.getElementById('dailys').style.color = 'hsl(236, 100%, 87%)';
+    document.getElementById('weeklys').style.color = 'hsl(236, 100%, 87%)';
+    document.getElementById('monthlys').style.fontWeight='600';
+    document.getElementById('monthlys').style.color = 'white';
+    document.getElementById('use1').innerHTML=" 103hrs ";
+    document.getElementById('used1').innerHTML="Previous - 128hrs ";
+    document.getElementById('use2').innerHTML="23hrs";
+    document.getElementById('used2').innerHTML=" Previous - 29hrs";
+    document.getElementById('use3').innerHTML="13hrs";
+    document.getElementById('used3').innerHTML="Previous - 19hrs";
+    document.getElementById('use4').innerHTML="11hrs";
+    document.getElementById('used4').innerHTML="Previous - 18hrs";
+    document.getElementById('use5').innerHTML="21hrs";
+    document.getElementById('used5').innerHTML="Previous - 23hrs";
+    document.getElementById('use6').innerHTML="7hrs";
+    document.getElementById('used6').innerHTML="Previous - 11hrs";
+
+}
+
